9692618ba9c27453e264301beb5370dd4dd6836f
Pragati Singh <pragatisingh933585@gmail.com>
2026-01-21 22:34:33 +0530

    Implement auth, credits, and image generation APIs

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ import express from 'express'
 const userRouter= express.Router()
 userRouter.post('/register', registerUser)
 userRouter.post('/login' , loginUser)
-userRouter.post('/credits' , userAuth,userCredits)
+userRouter.get('/credits' , userAuth,userCredits)
 
 export default userRouter
 //http://localhost:4000/api/user/register
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import userRouter from './routes/userRoutes.js'
 
 import connectDB from './config/mongodb.js'
+import imageRouter from './routes/imageRouter.js'
 const app = express()
 const PORT = process.env.PORT || 4000
 
@@ -11,7 +12,8 @@ app.use(express.json())
 app.use(cors())
 
 connectDB()
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter)
+app.use('/api/image', imageRouter)
 
 app.get('/', (req, res) => {
   res.send("API Working")
